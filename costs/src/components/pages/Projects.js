312aa446d1471365d0308bb1131e8ca3e327c483
Igor-Maceno <igorlopesmaceno17@gmail.com>
2022-01-20 22:05:45 -0300

    System msg
    Criação e configuração das mensagens do sistema.

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,7 +1,25 @@
+import {useLocation} from 'react-router-dom'
+import styles from './Projects.module.css';
+import Message from '../layouts/Message';
+import Container from '../layouts/Container';
+import LinkButton from '../layouts/LinkButton';
+
 function Projects(){
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
     return(
-        <div>
-            <h1>Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.tittle_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
+            {Message && <Message msg={message} type="success"/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
